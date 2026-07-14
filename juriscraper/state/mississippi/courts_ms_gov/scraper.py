71b3bf4dfb0dac65049adf3eb71f7cf69a67df02
Brennan Chesley <brennan@free.law>
2026-07-14 16:46:32 -0500

    ms spec import fix

--- juriscraper/state/mississippi/courts_ms_gov/scraper.py
+++ juriscraper/state/mississippi/courts_ms_gov/scraper.py
@@ -38,7 +38,6 @@ from datetime import date
 from typing import TYPE_CHECKING, ClassVar
 
 from jkent.common.decorators import entry, step
-from jkent.common.param_models import PersistentSpeculativeRange
 from jkent.data_types import (
     BaseScraper,
     DriverRequirement,
@@ -50,6 +49,8 @@ from jkent.data_types import (
     ScraperStatus,
 )
 from pyrate_limiter import Duration, Rate
+
+from juriscraper.state.common import PersistentSpeculativeRange
 
 from .models import (
     BUILD_DOCKET_URL,
